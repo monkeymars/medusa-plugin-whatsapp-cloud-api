c4e83e30988d709a6bc5bf729e5432afdd8ddd09
Gery Wibowo <gery.wibowo@lemonilo.com>
2022-10-16 22:49:27 +0700

    lint: double quote

--- medusa-config.js
+++ medusa-config.js
@@ -1,29 +1,29 @@
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
-let ENV_FILE_NAME = '';
+let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
-	case 'production':
-		ENV_FILE_NAME = '.env.production';
-		break;
-	case 'staging':
-		ENV_FILE_NAME = '.env.staging';
-		break;
-	case 'test':
-		ENV_FILE_NAME = '.env.test';
-		break;
-	case 'development':
-	default:
-		ENV_FILE_NAME = '.env';
-		break;
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
 }
 
 try {
-	dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
-} catch (e) {
-}
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
@@ -39,10 +39,22 @@ const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
 const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
 
+// WhatsApp API
+// const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN || "secret";
+// const WHATSAPP_WEBHOOK_VERIFY_TOKEN =
+//   process.env.WHATSAPP_WEBHOOK_VERIFY_TOKEN || "secret";
+
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
+  // {
+  //   resolve: `medusa-whatsapp-cloud-api`,
+  //   options: {
+  //     whatsappToken: WHATSAPP_TOKEN,
+  //     whatsappWebhookVerifyToken: WHATSAPP_WEBHOOK_VERIFY_TOKEN,
+  //   },
+  // },
   // Uncomment to add Stripe support.
   // You can create a Stripe account via: https://stripe.com
   // {
